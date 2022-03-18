9305e51063f80e5460f6a16ed4544a8d1721cc80
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2022-03-17 21:30:13 -0300

    alterando ponto para espaço vazio

--- anagram.js
+++ anagram.js
@@ -1,6 +1,6 @@
 function isAnagram(str1,str2){
-str1 = str1.toString().replace('.','').toLowerCase().split('').sort().join('');
-str2 = str2.toString().replace('.','').toLowerCase().split('').sort().join('');
+str1 = str1.toString().toReplace('.','').replace('.','').toLowerCase().split('').sort().join('');
+str2 = str2.toString().toReplace('.','').replace('.','').toLowerCase().split('').sort().join('');
 return str1 === str2;
 }
 module.exports = isAnagram;
