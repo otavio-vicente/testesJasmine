06896af94549644538b3ad8e4ca0330d4b87e9ef
otavio-vicente <otaviohvcnt@gmail.com>
2022-03-23 22:05:45 -0300

    atualização de matchers

--- spec/matchers-spec.js
+++ spec/matchers-spec.js
@@ -31,3 +31,90 @@ describe("Suíte de testes do toEqual", function(){
     expect(obj1).not.toEqual(obj2);
     });
 });
+
+//toMatch
+
+describe("Suíte de testes do toMatch", function(){
+    var textoComparar = "Curso de testes com Jasmine";
+    it("deve validar o uso do matcher 'toMatch", function(){
+        expect(textoComparar).toMatch(/Jasmine/);
+        expect(textoComparar).toMatch("Jasmine");
+        expect(textoComparar).toMatch(/jasmine/i);
+        expect(textoComparar).not.toMatch(/Javascript/);
+        expect("14/12/2016").toMatch(/^\d{2}\/\d{2}\/\d{4}$/);
+    });
+});
+
+//toBeDefined
+
+describe("Suíte de testes do toBeDefined", function(){
+    var numero = 10;
+    var texto;
+    var obj = { 'valor' : 10 }; 
+    it("deve validar o uso do matcher 'toBeDefined'", function(){
+        expect(numero).toBeDefined();
+        expect(texto).not.toBeDefined();
+        expect(obj.valor).toBeDefined();
+        expect(obj.mensagem).not.toBeDefined();
+    });
+});
+
+//toBeUndefined
+
+describe("Suíte de testes do toBeUndefined", function(){
+    var numero = 10;
+    var texto;
+    var obj = { 'valor' : 10 }; 
+    it("deve validar o uso do matcher 'toBeUndefined'", function(){
+        expect(texto).toBeUndefined();
+        expect(numero).not.toBeUndefined();
+        expect(obj.mensagem).toBeUndefined();
+        expect(obj.valor).not.toBeUndefined();
+    });
+});
+
+//toBeNull
+
+describe("Suíte de testes do toBeNull", function(){
+    var objeto = null;
+    var texto;
+    var numero = 10;
+
+    it("deve validar o uso do matcher 'toBeNull'", function(){
+        expect(objeto).toBeNull();
+        expect(numero).not.toBeNull();
+        expect(texto).not.toBeNull();
+        expect(objeto).not.toEqual(texto);
+    });
+});
+
+//toBeTruthy
+
+describe("Suíte de testes do toBeTruthy", function(){
+    var objeto = { 'valor': 123 };
+    var texto;
+    var numero = 10;
+    
+    it("deve validar o uso do matcher 'toBeTruthy'", function(){
+        expect(objeto).toBeTruthy();
+        expect(numero).toBeTruthy();
+        expect(texto).not.toBeTruthy();        
+    });
+});
+
+//toBeFalsy
+
+describe("Suíte de testes do toBeFalsy", function(){
+    var numero = 10;
+
+    it("deve validar o uso do matcher 'toBeFalsy'", function(){
+        expect(false).toBeFalsy();
+        expect("").toBeFalsy();
+        expect(0).toBeFalsy();
+        expect(undefined).toBeFalsy();
+        expect(null).toBeFalsy();
+        expect(NaN).toBeFalsy();
+        expect(numero).not.toBeFalsy();
+        expect("false").not.toBeFalsy();        
+    });
+});
